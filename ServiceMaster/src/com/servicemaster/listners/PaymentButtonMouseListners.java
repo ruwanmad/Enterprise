37d0602f4860d2e575a555ab1e001f6b089b1a96
RuwanM <RuwanM@RuwanM-PC>
2017-09-11 14:46:38 +0530

    Change button listener to handle mouse enter and exit events

--- ServiceMaster/src/com/servicemaster/listners/PaymentButtonMouseListners.java
+++ ServiceMaster/src/com/servicemaster/listners/PaymentButtonMouseListners.java
@@ -8,6 +8,7 @@ package com.servicemaster.listners;
 import com.servicemaster.data.SystemData;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 
 /**
@@ -31,18 +32,22 @@ public class PaymentButtonMouseListners implements MouseListener {
     @Override
     public void mouseEntered(MouseEvent e) {
         Object object = e.getSource();
-        if (object instanceof JLabel) {
-            JLabel label = (JLabel) object;
-            label.setBackground(SystemData.MOUSE_ENTER_COLOR);
+        if (object instanceof JButton) {
+            JButton button = (JButton) object;
+            if (button.isEnabled()) {
+                button.setBackground(SystemData.MOUSE_ENTER_COLOR);
+            }
         }
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
         Object object = e.getSource();
-        if (object instanceof JLabel) {
-            JLabel label = (JLabel) object;
-            label.setBackground(SystemData.MOUSE_EXIT_COLOR);
+        if (object instanceof JButton) {
+            JButton button = (JButton) object;
+            if (button.isEnabled()) {
+                button.setBackground(SystemData.MOUSE_EXIT_COLOR);
+            }
         }
     }
 
